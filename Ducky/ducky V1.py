de762f1d12731efa27bf8bb136c33a98ed291947
Mr1Skeletoner <glitchtrapissus@gmail.com>
2026-03-22 18:10:05 +0200

    the program
    the program

---
+++ Ducky/ducky V1.py
@@ -0,0 +1,38 @@
+#fk her
+
+
+from tkinter import *
+import winsound
+import time
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__))) # i dont know what this does
+# but it fixes the problem where the code looks for the file in another place
+
+ducky_window = Tk()
+
+ducky_window.geometry("250x300")
+ducky_window.title("Ducky")
+ducky = PhotoImage(file="ducky.png")
+ducky_window.iconphoto(True, ducky)
+
+ducky_says = StringVar()
+ducky_says.set("")
+
+def ducky_clicked():
+    winsound.PlaySound("quack.wav", 0) # shoutout to crazyduckman or smth
+    ducky_says.set("I believe in you!")
+
+ducky_button = Button(ducky_window, image=ducky, command=ducky_clicked)
+ducky_text = Label(ducky_window, textvariable=ducky_says)
+
+
+ducky_button.pack()
+ducky_text.pack()
+
+
+ducky_window.mainloop()
+
+
+
+# i feel stupid making ts
+# no way this took 3 hours
